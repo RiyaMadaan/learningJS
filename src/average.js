0cff20339d9518d4e6a332110ae0e62d1f4ad568
Riya <riyamadaan404@gmail.com>
2020-04-02 23:14:53 +0530

    finding the average of three numbers

---
+++ src/average.js
@@ -0,0 +1,8 @@
+let number1 = +process.argv[2];
+let number2 = +process.argv[3];
+let number3 = +process.argv[4];
+
+sum = number1 + number2 + number3;
+console.log(sum);
+average = sum/3;
+console.log("average is", average);
