f81fcd7cd4e267fdb3246bacdc5d274db205d7ab
Riya <riyamadaan404@gmail.com>
2020-04-09 00:02:34 +0530

    even number between two numbers

--- src/EvenNumbers.js
+++ src/EvenNumbers.js
@@ -1,11 +1,10 @@
 let number1 = +process.argv[2];
 let number2 = +process.argv[3];
-
-let i =2;
-let range = number2 -2;
-do{
-  number = number1 + i;
-  console.log(number);
-  i = i + 2;
+ if(number1%2==0){
+let count = 0;
+if(number1 < number2){
+for(count = number1 +2; count < number2 ; count = count+2){
+  console.log(count);
 }
-while(i<range)
+}
+}
